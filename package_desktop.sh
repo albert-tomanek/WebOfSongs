85114108019224dd95f35ce23bcf23dfc593f0ef
albert-tomanek <electron826@gmail.com>
2024-03-20 01:02:01 +0000

    Packaging as desktop app now possible.

--- package_desktop.sh
+++ package_desktop.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 #neu create distrib
 npm run build
+
+rm -rf distrib/app/
+mkdir -p distrib/app/
+cp -r build/* distrib/app/
+
+#git clone https://github.com/hschneider/neutralino-build-scripts.git --depth 1
+sh -c "cd distrib; ../neutralino-build-scripts/build-win.sh"
+sh -c "cd Neu-AppImage/;npm run-script test -- --exe ../distrib/dist/webofsongs/webofsongs-linux_x64 --res ../distrib/dist/webofsongs/resources.neu --program-name WebOfSongs --description \"Organize your Spotify songs into a mindmap-like web.\" --icon ../distrib/app/logo512.png --categories "Audio" --arch "x86_64" --out-dir .."
